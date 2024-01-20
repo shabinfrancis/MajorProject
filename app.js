7fa3accd2dacfc568c958b4f1fecf3a932c45700
Shabin Francis <shabinfrancis25@gmail.com>
2024-01-20 10:02:18 +0530

    Made changes in DB Url in app.js

--- app.js
+++ app.js
@@ -43,7 +43,7 @@ app.engine("ejs", ejsMate);
 app.use(express.static(path.join(__dirname, "/public")));
 
 const store = MongoStore.create({
-  mongoUrl: MONGO_URL,
+  mongoUrl: dbUrl,
   crypto: {
     secret: process.env.ATLASDB_URL,
   },
